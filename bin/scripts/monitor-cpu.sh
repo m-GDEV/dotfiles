6fcd3b19590b6d72e3aeb61786c16a9b9e092bed
Musa Ahmed <musaa.ahmed7@gmail.com>
2022-09-28 04:50:27 -0400

    28-09-22

--- bin/scripts/monitor-cpu.sh
+++ bin/scripts/monitor-cpu.sh
@@ -5,7 +5,7 @@
 # 2: It calculates the max and min points of the cpu's temp while the script has been running
 # 3: It finds the average temp & cpu usage
 
-rm ./cpu.log
+rm /tmp/cpu.log
 START=$(date)
 SECS=$(date +%s)
 # set -xv
@@ -19,24 +19,24 @@ while [ 7 -gt 6 ]; do
     CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
     TEMP=$(cat /sys/class/thermal/thermal_zone0/temp /sys/class/thermal/thermal_zone1/temp | tail -1)
 
-    echo "$(date) | $CPU - $(( TEMP / 1000))" >> cpu.log
+    echo "$(date) | $CPU - $(( TEMP / 1000))" >> /tmp/cpu.log
 
-    MAX_TEMP=$(cat ./cpu.log | awk '{print $11}' | sort -n | tail -1)
-    MIN_TEMP=$(cat ./cpu.log | awk '{print $11}' | sort -n | head -1)
+    MAX_TEMP=$(cat /tmp/cpu.log | awk '{print $11}' | sort -n | tail -1)
+    MIN_TEMP=$(cat /tmp/cpu.log | awk '{print $11}' | sort -n | head -1)
     
     # its gonna be 0 and 99 all the time so no point
     # MAX_CPU=$(cat ./cpu.log | awk '{print $9}' | cut -c 1-2 | tr '.' ' ' | tr '%' ' ' | sort -n | tail -1)
     # MIN_CPU=$(cat ./cpu.log | awk '{print $9}' | cut -c 1-2 | tr '.' ' ' | tr '%' ' ' | sort -n | head -1 | rev | cut -c 2- | rev)
 
-    NUMS=$(cat ./cpu.log | wc -l)
+    NUMS=$(cat /tmp/cpu.log | wc -l)
    
     # TEMPERATURES
-    TEMPS=$(cat ./cpu.log | awk '{print $11}')
+    TEMPS=$(cat /tmp/cpu.log | awk '{print $11}')
     for i in $TEMPS; do TOTAL_TEMP=$(($TOTAL_TEMP + $i)); done
     AVG_TEMP=$(($TOTAL_TEMP / $NUMS))
 
     # PERCENTAGES
-    PERS=$(cat ./cpu.log | awk '{print $9}' | cut -c 1-2 | tr '.' ' ' | tr '%' ' ')
+    PERS=$(cat /tmp/cpu.log | awk '{print $9}' | cut -c 1-2 | tr '.' ' ' | tr '%' ' ')
     for i in $PERS; do TOTAL_PER=$(($TOTAL_PER + $i)); done
     AVG_PER=$(($TOTAL_PER / $NUMS))
    
